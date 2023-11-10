5bbf7c3e963e7440ccff5968f4a81add722fb6ab
Bonecide <marselkasymov23@gmail.com>
2023-11-10 22:58:22 +0600

    preloade 3sec => 2sec

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false)
-    }, 3000)
+    }, 2000)
   }, [])
 
   const loaderTransition = useTransition(isLoading, {
