ba59b9da4719e11552c3508163af2192a4eb9268
Bonecide <marselkasymov23@gmail.com>
2023-11-10 22:44:27 +0600

    added preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {useEffect, useState} from 'react'
 import {AboutMe} from './features/AboutMe/AboutMe'
 import {Footer} from './features/Footer/Footer'
 import {Header} from './features/Header/Header'
@@ -5,10 +6,47 @@ import {Projects} from './features/Projects/Projects'
 import {Skills} from './features/Skills/Skills'
 import {WelcomePage} from './features/WelcomePage/WelcomePage'
 import './global.scss'
+import {Loader} from './components/Loader/Loader'
+import {animated, useTransition} from '@react-spring/web'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+  }, [])
+
+  const loaderTransition = useTransition(isLoading, {
+    from: {opacity: 0, background: '#1a1a1a'},
+    enter: {opacity: 1},
+    leave: {opacity: 0},
+    config: {duration: 300}, // Adjust the duration for a smoother transition
+  })
+
   return (
     <div>
+      {loaderTransition(
+        (style, item) =>
+          item && (
+            <animated.div
+              style={{
+                ...style,
+                display: 'flex',
+                position: 'fixed',
+                justifyContent: 'center',
+                alignItems: 'center',
+               
+                zIndex: 9999999,
+                width: '100vw',
+                height: '100vh',
+              }}
+            >
+              <Loader />
+            </animated.div>
+          )
+      )}
       <Header />
       <WelcomePage />
       <AboutMe />
